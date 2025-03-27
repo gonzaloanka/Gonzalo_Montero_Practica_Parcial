a0e3d58395fe72f9eddeaf1038a49e460f9155f1
gonzaloanka <149703470+gonzaloanka@users.noreply.github.com>
2025-03-27 11:40:46 +0100

    Create user.routes.js

---
+++ routes/user.routes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+// Controlador placeholder
+router.post('/register', (req, res) => {
+  res.send('Registro de usuario');
+});
+
+module.exports = router;
