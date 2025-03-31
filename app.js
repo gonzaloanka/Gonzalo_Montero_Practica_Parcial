8338737d4dd87d534b8f3c74078cf09839f26ff2
gonzaloanka <149703470+gonzaloanka@users.noreply.github.com>
2025-03-31 17:03:55 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,14 +6,11 @@ const connectDB = require('./config/mongo');
 dotenv.config();
 const app = express();
 
-// Middlewares
 app.use(cors());
 app.use(express.json());
 
-// Rutas
 app.use('/api/user', require('./routes/user.routes'));
 
-// Conexión DB y arranque
 connectDB();
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
